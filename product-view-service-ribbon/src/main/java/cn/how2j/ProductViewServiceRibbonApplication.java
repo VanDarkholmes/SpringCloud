e176c773fae196d1d0a61e6c488acd6e68f219a6
keke32374@qq.com <keke32374@qq.com>
2020-08-07 16:44:38 +0800

    SpringCloud 0.2

--- product-view-service-ribbon/src/main/java/cn/how2j/ProductViewServiceRibbonApplication.java
+++ product-view-service-ribbon/src/main/java/cn/how2j/ProductViewServiceRibbonApplication.java
@@ -27,36 +27,36 @@ public class ProductViewServiceRibbonApplication {
 
     public static void main(String[] args) {
         int port = 0;
-        int defaultPort = 8010;
-        Future<Integer> future = ThreadUtil.execAsync(() ->{
-            int p = 0;
-            System.out.println("请于5秒钟内输入端口号, 推荐  8010  超过5秒将默认使用 " + defaultPort);
-            Scanner scanner = new Scanner(System.in);
-            while(true) {
-                String strPort = scanner.nextLine();
-                if(!NumberUtil.isInteger(strPort)) {
-                    System.err.println("只能是数字");
-                    continue;
-                }
-                else {
-                    p = Convert.toInt(strPort);
-                    scanner.close();
-                    break;
-                }
-            }
-            return p;
-        });
-        try{
-            port=future.get(5,TimeUnit.SECONDS);
-        }
-        catch (InterruptedException | ExecutionException | TimeoutException e){
-            port = defaultPort;
-        }
-        if(!NetUtil.isUsableLocalPort(port)) {
-            System.err.printf("端口%d被占用了，无法启动%n", port );
+        int defaultPort = 8013;
+//        Future<Integer> future = ThreadUtil.execAsync(() ->{
+//            int p = 0;
+//            System.out.println("请于5秒钟内输入端口号, 推荐  8010  超过5秒将默认使用 " + defaultPort);
+//            Scanner scanner = new Scanner(System.in);
+//            while(true) {
+//                String strPort = scanner.nextLine();
+//                if(!NumberUtil.isInteger(strPort)) {
+//                    System.err.println("只能是数字");
+//                    continue;
+//                }
+//                else {
+//                    p = Convert.toInt(strPort);
+//                    scanner.close();
+//                    break;
+//                }
+//            }
+//            return p;
+//        });
+//        try{
+//            port=future.get(5,TimeUnit.SECONDS);
+//        }
+//        catch (InterruptedException | ExecutionException | TimeoutException e){
+//            port = defaultPort;
+//        }
+        if(!NetUtil.isUsableLocalPort(defaultPort)) {
+            System.err.printf("端口%d被占用了，无法启动%n", defaultPort );
             System.exit(1);
         }
-        new SpringApplicationBuilder(ProductViewServiceRibbonApplication.class).properties("server.port=" + port).run(args);
+        new SpringApplicationBuilder(ProductViewServiceRibbonApplication.class).properties("server.port=" + defaultPort).run(args);
 
     }
     @Bean
